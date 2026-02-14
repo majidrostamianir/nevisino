a93dba8a0a7397671f01f7a30081a55eec3d84b0
Majid Rostamian <pofelak@gmail.com>
2026-02-15 01:17:23 +0330

    improve visits

--- app/Http/Middleware/visitTracker.php
+++ app/Http/Middleware/visitTracker.php
@@ -13,13 +13,56 @@ class visitTracker
     public function handle(Request $request, Closure $next): Response
     {
         try {
+
+            $userAgent = $request->userAgent();
             $ip = $request->ip();
+
+            // تشخیص ربات
+            $isBot = false;
+            if ($userAgent) {
+                $botKeywords = ['bot', 'crawl', 'slurp', 'spider', 'mediapartners'];
+                foreach ($botKeywords as $keyword) {
+                    if (stripos($userAgent, $keyword) !== false) {
+                        $isBot = true;
+                        break;
+                    }
+                }
+            }
+
+            // رنج آی‌پی‌هایی که نمیخوایم ذخیره بشن
+            $blockedIpRanges = [
+                '81.12.31.*', // همه آی‌پی‌های 81.12.31.0 تا 81.12.31.255
+            ];
+
+            // یوزر ایجنت‌های بلاک
+            $blockedUserAgents = [
+                'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/116.0.1938.76 Safari/537.36',
+                'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 (compatible; Google-Read-Aloud; +https://support.google.com/webmasters/answer/1061943)'
+            ];
+
+            // چک کردن رنج آی‌پی
+            foreach ($blockedIpRanges as $range) {
+                if (fnmatch($range, $ip)) {
+                    return $next($request); // ذخیره نکن
+                }
+            }
+
+            // چک کردن یوزر ایجنت
+            foreach ($blockedUserAgents as $blockedUA) {
+                if (stripos($userAgent, $blockedUA) !== false) {
+                    return $next($request); // ذخیره نکن
+                }
+            }
+
+            // ذخیره بازدید برای کاربران عادی
             if (Auth::guest() || (Auth::check() && Auth::user()->id != 1)) {
                 Visit::query()->create([
                     'ip' => $ip ?? null,
                     'user_id' => auth()->id() ?? null,
                     'url' => urldecode($request->path() ?? ''),
                     'referrer' => $request->headers->get('referer') ? urldecode($request->headers->get('referer')) : null,
+                    'is_bot' => $isBot,
+                    'user_agent' => $userAgent,
                 ]);
             }
         } catch (\Throwable $e) {
@@ -28,4 +71,5 @@ class visitTracker
 
         return $next($request);
     }
+
 }
--- resources/views/livewire/admin/visit/index.blade.php
+++ resources/views/livewire/admin/visit/index.blade.php
@@ -4,13 +4,17 @@
             <thead class="bg-gray-100">
             <tr>
                 <th class="px-4 py-2 ">
-                    <input wire:model.live.debounce.500ms="queryIp" class="rounded-2xl bg-white h-8 px-2" placeholder="جستجوی آیپی ...">
+                    <input wire:model.live.debounce.500ms="queryIp" class="rounded-2xl bg-white h-8 px-2"
+                           placeholder="جستجوی آیپی ...">
                 </th>
                 <th class="px-4 py-2 ">
-                    <input wire:model.live.debounce.500ms="queryUser" class="rounded-2xl bg-white h-8 px-2" placeholder="جستجوی کاربر ...">
+                    <input wire:model.live.debounce.500ms="queryUser" class="rounded-2xl bg-white h-8 px-2"
+                           placeholder="جستجوی کاربر ...">
                 </th>
                 <th class="px-4 py-2 ">صفحه</th>
+                <th class="px-4 py-2 ">بات</th>
                 <th class="px-4 py-2 ">رفرر</th>
+                <th class="px-4 py-2 ">ایجنت</th>
                 <th class="px-4 py-2 ">زمان</th>
             </tr>
             </thead>
@@ -20,10 +24,13 @@
                     <td class="px-4 py-2 ">{{ $item->ip }}</td>
                     <td class="px-4 py-2 ">{{ \App\Models\User::find($item->user_id)->name ?? \App\Models\User::find($item->user_id)->mobile ?? null }}</td>
                     <td class="px-4 py-2 ">{{ \App\Models\Url::find($item->url_id)->title ?? $item->url }}</td>
-                    <td class="px-4 py-2 ">{{ $item->referrer }}</td>
+                    <td class="px-4 py-2 ">{{ $item->is_bot }}</td>
+                    <td class="px-4 py-2 w-8">{{ $item->referrer }}</td>
+                    <td class="px-4 py-2 ">{{ $item->user_agent }}</td>
                     <td class="px-4 py-2 ">{{ english_to_persian_num(verta($item->created_at)) }}</td>
                 </tr>
             @endforeach
             </tbody>
         </table>
-    </div></div>
+    </div>
+</div>
